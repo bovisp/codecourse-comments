61e67a4ccbc77ebdc55e54e6da400b3c7b36ed8d
Paul Bovis <paulbovisblahblah@gmail.com>
2018-09-19 10:03:23 -0600

    Ad Comment and User resources and endpoint for course comments

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\CommentResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'body' => $this->body,
+            'children' => CommentResource::collection(
+                $this->whenLoaded('children')
+            ),
+            'user' => new UserResource($this->user)
+        ];
+    }
+}
