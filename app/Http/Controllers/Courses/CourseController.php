b09e45069f30063fc9398db1f85d7ed906ec5615
Paul Bovis <paulbovisblahblah@gmail.com>
2018-09-19 13:07:24 -0600

    Setup Comments component

--- app/Http/Controllers/Courses/CourseController.php
+++ app/Http/Controllers/Courses/CourseController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Courses;
 
 use App\Course;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CourseController extends Controller
 {
     public function show(Course $course)
     {
-    	return view('courses.show');
+    	return view('courses.show', compact('course'));
     }
 }
---
+++ resources/js/components/comments/NewComment.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="mb-5">
+		<button 
+			class="btn btn-primary btn-block"
+			@click.prevent="active = true"
+			v-if="!active"
+		>Add new comment</button>
+
+		<template v-else>
+			<form @submit.prevent="store">
+				<div class="form-group">
+					<label for="body">Comment</label>
+
+					<textarea
+						id="body"
+						rows="10"
+						class="form-control"
+						autofocus="autofocus"
+						v-model="form.body"
+					></textarea>
+
+					<p class="form-text text-muted">
+						Markdown and code highlighting are supported
+					</p>
+				</div>
+
+				<div class="form-group">
+					<button 
+						class="btn btn-primary" 
+						type="submit"
+					>Post</button>
+
+					<button 
+						class="btn btn-link"
+						@click.prevent="active = false"
+					>Cancel</button>
+				</div>
+			</form>
+		</template>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				active: false,
+				form: {
+					body: ''
+				}
+			}
+		},
+
+		props: {
+			endpoint: {
+				required: true,
+				type: String
+			}
+		},
+
+		methods: {
+			store () {
+				console.log('store', this.form.body)
+			}
+		}
+	}
+</script>
