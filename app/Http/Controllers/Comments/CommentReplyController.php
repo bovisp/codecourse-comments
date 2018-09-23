49365ba202caed7fd2d2bce6a204a4e5927c1728
Paul Bovis <paulbovisblahblah@gmail.com>
2018-09-23 13:35:45 -0600

    Create CommentReplyController to reply to a comment

---
+++ app/Http/Controllers/Comments/CommentReplyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Comments;
+
+use App\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
+
+class CommentReplyController extends Controller
+{
+    public function store(Comment $comment, Request $request)
+    {
+    	\Auth::loginUsingId(1);
+    	$this->authorize('reply', $comment);
+
+    	$request->validate([
+    		'body' => 'required|max:5000'
+    	]);
+
+    	$reply = $comment->children()->make([
+    		'body' => $request->body
+    	]);
+
+    	$reply->commentable()->associate($comment->commentable);
+
+    	$request->user()->comments()->save($reply);
+
+    	return new CommentResource($reply);
+    }
+}
